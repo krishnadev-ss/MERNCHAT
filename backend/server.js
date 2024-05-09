6df52a1b888d211743615caf23a650de3f6da187
krishnadev-ss <krishnadevsreekumar2002@gmail.com>
2024-05-09 15:30:47 +0530

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const express = require("express");
-
+const {chats} = require("./data/data");
 const app = express();
+const dotenv = require("dotenv");
 
-app.listen(5000,console.log("server started on port 5000"));
+dotenv.config();
+app.get("/", (req, res) => {
+    res.send("Api is running successfully");
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singlechat = chats.find((c)=>c._id === req.params.id);
+    res.send(singlechat);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`server started on port ${PORT}`));
