ffbe10f968cbb653fd2a751c278ac58b3ee2c9f6
krishnadev-ss <krishnadevsreekumar2002@gmail.com>
2024-05-24 19:05:28 +0530

    message route backend

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,53 @@
 const express = require("express");
-const {chats} = require("./data/data");
-const app = express();
-const dotenv = require("dotenv");
+const colors = require('colors'); // Ensure you have this line at the top of your file
+
 const connectDB = require("./config/db");
-const colors = require("colors");
+const dotenv = require("dotenv");
 const userRoutes = require("./Routes/userRoutes");
 const chatRoutes = require("./Routes/chatRoutes");
-
-
-
-const { notFound, errorHandler } = require('./middleware/errorMiddleware');
-
-const myMiddleware = (req, res, next) => {
-  notFound(req, res, next); // Pass req object
-  errorHandler(new Error('Not Found'), req, res, next);
-};
-
+const messageRoutes = require("./Routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
 dotenv.config();
 connectDB();
+const app = express();
 
-app.use(express.json()); //to accept json data
-app.get("/", (req, res) => {
-    res.send("Api is running successfully");
-})
+app.use(express.json()); // to accept json data
 
-app.use('/api/user',userRoutes)
-app.use('/api/chat',chatRoutes)
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
 
-app.use(myMiddleware);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
+// --------------------------deployment------------------------------
 
-const PORT = process.env.PORT || 5000
+const __dirname1 = path.resolve();
 
-app.listen(PORT,console.log(`server started on port ${PORT} `.yellow.bold));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
